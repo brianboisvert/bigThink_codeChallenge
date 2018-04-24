92f01da043f07e4cd2d2ff8b391a822764e3354b
brianboisvert <b.m.boisvert@gmail.com>
2018-04-23 21:35:06 -0400

    added data method

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,19 +3,26 @@ class Video < ApplicationRecord
   has_many :users, through: :usages
 
   def data
-    final = [0, 0, 0, 0]
+    final = [["25%", 0], ["50%", 0], ["75%", 0], ["100%", 0]]
     self.usages.each do |usage|
-      puts usage.percent
-      if usage.percent > 0 && usage.percent <= 25
-        final[0] += 1
-      elsif usage.percent > 25 && usage.percent <= 50
-        final[1] += 1
+      if usage.percent > 75 && usage.percent <= 100
+        final[0][1] += 1
+        final[1][1] += 1
+        final[2][1] += 1
+        final[3][1] += 1
       elsif usage.percent > 50 && usage.percent <= 75
-        final[2] += 1
-      elsif usage.percent > 75 && usage.percent <= 100
-        final[3] += 1
+        final[0][1] += 1
+        final[1][1] += 1
+        final[2][1] += 1
+       elsif usage.percent > 25 && usage.percent <= 50
+         final[0][1] += 1
+         final[1][1] += 1
+       elsif usage.percent > 0 && usage.percent <= 25
+         final[0][1] += 1
       end
     end
+
+
     return final
   end
 end
